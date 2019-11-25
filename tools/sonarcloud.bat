14bf13b0ffe768b9022145d954edf91f6ebbb783
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-11-25 23:00:32 +0300

    Register project at SonarCloud.

---
+++ tools/sonarcloud.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+SET JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_202
+SET ANDROID_SDK_ROOT=D:\Users\%USERNAME%\Documents\Android\android-sdk
+SET ANDROID_NDK_ROOT=D:\Users\%USERNAME%\Documents\Android\android-ndk-r19c
+SET SONARCLOUD_ROOT=D:\Users\%USERNAME%\Documents\SonarCloud
+SET ANDROID_PLATFORM=android-28
+
+PATH=%PATH%;C:\Qt\5.12.6\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows-x86_64\bin;%SONARCLOUD_ROOT%\build-wrapper-win-x86;%SONARCLOUD_ROOT%\sonar-scanner-3.3.0.1492-windows\bin
+
+IF NOT "%~1"=="" (
+        CD .. ^
+     && (IF NOT EXIST .sonarbuild (MKDIR .sonarbuild && ATTRIB +H .sonarbuild) || CD .) ^
+     && CD .sonarbuild ^
+     && qmake.exe ..\pocketpiglet.pro ^
+     && build-wrapper-win-x86-64.exe --out-dir bw-output make.exe clean all install INSTALL_ROOT=android-build ^
+     && androiddeployqt.exe --input android-libpocketpiglet.so-deployment-settings.json --output android-build --android-platform %ANDROID_PLATFORM% --deployment bundled --gradle --no-gdbserver ^
+     && CD .. ^
+     && sonar-scanner.bat -D"sonar.projectKey=pocketpiglet_pocketpiglet-android" ^
+                          -D"sonar.projectName=PocketPiglet Android" ^
+                          -D"sonar.organization=pocketpiglet-mobile-github" ^
+                          -D"sonar.sources=." ^
+                          -D"sonar.sourceEncoding=UTF-8" ^
+                          -D"sonar.exclusions=qml/**/*,translations/*,webrtc/**/*" ^
+                          -D"sonar.cfamily.build-wrapper-output=.sonarbuild\\bw-output" ^
+                          -D"sonar.java.source=1.7" ^
+                          -D"sonar.java.binaries=.sonarbuild" ^
+                          -D"sonar.host.url=https://sonarcloud.io" ^
+                          -D"sonar.login=%~1"
+) ELSE (
+    ECHO Syntax: sonarcloud.bat SONARCLOUD_KEY
+)
