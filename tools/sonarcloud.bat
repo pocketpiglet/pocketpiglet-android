ea85d3f0567d40de477afd3d20ab9ddb507d6679
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-07-31 13:18:17 +0300

    Add some quoting in scripts where appropriate.

--- tools/sonarcloud.bat
+++ tools/sonarcloud.bat
@@ -19,7 +19,7 @@ IF NOT "%~1"=="" (
      && make.exe clean ^
      && build-wrapper-win-x86-64.exe --out-dir bw-output make.exe -j%NUMBER_OF_PROCESSORS% all ^
      && make.exe install INSTALL_ROOT=android-build ^
-     && androiddeployqt.exe --input android-libpocketpiglet.so-deployment-settings.json --output android-build --android-platform %ANDROID_PLATFORM% --deployment bundled --gradle --no-gdbserver ^
+     && androiddeployqt.exe --input android-libpocketpiglet.so-deployment-settings.json --output android-build --android-platform "%ANDROID_PLATFORM%" --deployment bundled --gradle --no-gdbserver ^
      && CD .. ^
      && sonar-scanner.bat -D"sonar.projectKey=pocketpiglet-mobile_pocketpiglet-android" ^
                           -D"sonar.projectName=PocketPiglet Android" ^
